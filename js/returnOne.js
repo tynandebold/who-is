34ee976037f9cc190a4c1c351ced6218d703e511
Tynan DeBold <tynan.debold@tradeshift.com>
2019-01-10 11:30:20 +0100

    formatting attachement; adding in email to img request

--- js/returnOne.js
+++ js/returnOne.js
@@ -4,6 +4,7 @@ module.exports = (req, slackRes, result) => {
   const name = result[0]['Team Member: Full Name'];
   const jobTitle = result[0].Role;
   const email = result[0]['Company Email'];
+  const emailFormatted = `${result[0]['Company Email']}\n–––––––––––––––––––––––––––––––––––––––`;
 
   let fields = [];
   for (const key of Object.keys(result[0])) {
@@ -20,7 +21,7 @@ module.exports = (req, slackRes, result) => {
     .filter(field => field.title !== 'Role');
 
   request
-    .get(`https://slack.com/api/users.lookupByEmail?token=${process.env.SLACK_TOKEN}&email=dov@dovfriedman.com`)
+    .get(`https://slack.com/api/users.lookupByEmail?token=${process.env.SLACK_TOKEN}&email=${email}`)
     .then(data => {
       const pictureUrl = data.body.user.profile.image_192;
 
@@ -30,7 +31,7 @@ module.exports = (req, slackRes, result) => {
             "color": "#cccccc",
             "pretext": `You searched for *${req.body.text}*. Here's what we found:`,
             "title": name,
-            "text": `_${jobTitle}_\n${email}`,
+            "text": `_${jobTitle}_\n${emailFormatted}`,
             fields,
             "thumb_url": pictureUrl
           }
